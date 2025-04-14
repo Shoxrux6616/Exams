5922a04f8071484a986437a39d8ec0e147b69822
Shoxrux6616 <shoxruxsoliqulov@gmail.com>
2025-04-14 10:11:16 +0500

    Start

---
+++ Exam4/Exam4modul/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam4modul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
